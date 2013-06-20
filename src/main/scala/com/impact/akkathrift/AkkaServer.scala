a0263d17f9400741f2196d0a1b8fa393cb3e8e21
Andrew Stanton <Refefer@gmail.com>
2013-06-20 14:47:50 -0400

    Convenience apply methods for AkkaServer

--- src/main/scala/com/impact/akkathrift/AkkaServer.scala
+++ src/main/scala/com/impact/akkathrift/AkkaServer.scala
@@ -15,10 +15,15 @@ class AkkaThriftServer(args:AkkaThriftServer.Args) extends TServer(args) {
 }
 
 object AkkaThriftServer extends AkkaThriftConfig {
-  class Args(val atss:AkkaThriftServerSocket) extends TServer.AbstractServerArgs[Args](atss) {
+  class Args(val atss:AkkaThriftServerSocket) extends TServer.AbstractServerArgs[Args](atss) 
+  object Args {
+    def apply(atss:AkkaThriftServerSocket) = new Args(atss)
   }
   private[this] lazy val actorSystem_ = ActorSystem("AkkaThrift", conf)
 
   def actorSystem = actorSystem_
 
+  def apply(args:AkkaThriftServer.Args) = {
+    new AkkaThriftServer(args)
+  }
 }
